557d8f93bd248e402fefd49795c5e21a182976c7
Raptor7742 <106883599+Raptor7742@users.noreply.github.com>
2023-01-01 19:12:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const playerInstance = jwplayer("player").setup({
         },
         {
           file:
-            "",
+            "https://m115.syncusercontent.com/mfs-60:334829af5e0a6970ffcc8f6a34f7200f=============================/p/Isekai.Meikyuu.De.Harem.Wo.S01e01.Unrated.Vostfr.1080P.Webrip.french-anime.com.mp4?allowdd=0&datakey=PQ4ZKRyWCCA/bBTxZKBQAHCpeXQ7Ic0V6IjNQ+BjppCeUrmOfjpbllYobTZYssVQ5XrRSd/R9QVXSOr+KzAWfxZ1hUa3DFREkA0BvpzMqv4dthr/Pjcg+/7tqJ6U1VEkuO+/FuQIAOYKJHWUHHLSq/0NCRtrZfArv/5ZlwciqnhcqsbFz3wQZphVvcbxnzJqV2fbsOgE+hFGseHeBHAcy83g8Nutj2igMvBl1lo+MSJ+4VOUpsPgx/C9DuuvDCivPkLLXK5mCPGSHief3S318KrkqtrMGSalnHp4YyaLM+8gpHyFvGlrlT3TSxuvIc0bT5/MkKEO1B5wRBzL5tbfEw&engine=ln-2.3.7.3&errurl=YkUlthAO8ujBrj1ch+IgWOZLYrnXaMS0j0/ksumUCWMYkPGfIpmAIfC/iO7TkN3ZhYcdy7+iSJPh/+gnS+m+IQwc/1Hvg+kdi9qpeuT353w7MkeKwnnUgsitYskBoeAA5zMnQDZLyLhP4nCuFNRNywAkhXa9qkfBSUdTCWQQcw2tVeWXxw/XRZvi4xJtU02hr/+oSrjdBFkvu8v6UCR8ySw10fgCGp6/BSi4DlNDolRWqNnXyJLNCaSgU1scVf83utX4etKMeTfkG7UMXPzuF/KN+VtRlTL/qqFIfMhADJA0NOK+5KLOyhnx99GrtNu/0euawtPFfBfMTa+K+5LZeA==&header1=Q29udGVudC1UeXBlOiB2aWRlby9tcDQ&header2=Q29udGVudC1EaXNwb3NpdGlvbjogaW5saW5lOyBmaWxlbmFtZT0iSXNla2FpLk1laWt5dXUuRGUuSGFyZW0uV28uUzAxZTAxLlVucmF0ZWQuVm9zdGZyLjEwODBQLldlYnJpcC5mcmVuY2gtYW5pbWUuY29tLm1wNCI7ZmlsZW5hbWUqPVVURi04JydJc2VrYWkuTWVpa3l1dS5EZS5IYXJlbS5Xby5TMDFlMDEuVW5yYXRlZC5Wb3N0ZnIuMTA4MFAuV2VicmlwLmZyZW5jaC1hbmltZS5jb20ubXA0Ow&ipaddress=1458994159&linkcachekey=765945ee0&linkoid=652350000&mode=101&sharelink_id=8937710510000&timestamp=1672596730535&uagent=220523ca5285197b0fad467e0e72e6907a6c5738&signature=1967e9f7f648ebf15fdcf7499bf720b552c51819&cachekey=60:334829af5e0a6970ffcc8f6a34f7200f=============================",
           label: "720p"
         },
         {
